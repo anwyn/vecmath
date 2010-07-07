32aadcdac0fdcbaf908995981f86d20066840196
Ole Arndt <anwyn@sugarshark.com>
2010-07-07 02:19:14 +0200

    Simplified macro expansion for vector functions.
    Do not emit the full body into each function but call the
    lower level functions from the other ones.

--- src/types.lisp
+++ src/types.lisp
@@ -204,14 +204,7 @@ and be of length (* dimension dimension)")))
                                     (ensure-cadr arg))) args)
                      ,return-type) ,name))))
 
-(defun emit-multiple-value-call-function (type name args doc body)
-  `((defun ,name ,(mapcar #'ensure-car args)
-      ,doc
-      (with-vectors ,args
-        (multiple-value-call #',type
-          (progn ,@body))))))
-
-(defun emit-with-vectors-function (name args doc body)
+(defun emit-vector-function (name args doc body)
   `((defun ,name ,(mapcar #'ensure-car args)
       ,doc
       (with-vectors ,args
@@ -221,14 +214,6 @@ and be of length (* dimension dimension)")))
   `((defun ,name ,(mapcar #'ensure-car args)
       ,doc
       ,@body)))
-
-(defun emit-destructive-function (name args doc body)
-  `((defun ,name ,(mapcar #'ensure-car args)
-      ,doc
-      (with-vectors ,args
-        (setf ,(cons 'values (expand-vector-arg (first args)))
-              (progn ,@body)))
-      ,(ensure-car (first args)))))
 
 ;;;; ---------------------------------------------------------------------------
 ;;;; * Vector function defining macro
@@ -250,54 +235,65 @@ and be of length (* dimension dimension)")))
         (mv-name (symbolicate name "%")))
     (flet ((option? (option default)
              (getf options option default)))
-      (cond ((eq type 'scalar)
-             (nconc (list 'progn)
-                    (emit-declarations type element-type name arglist :inline (option? :inline t))
-                    (emit-with-vectors-function name arglist doc body)
-                    (when (option? :scalar-args-version t)
-                      (let ((scalar-args (expand-vector-lambda-list arglist)))
+      (let ((scalar-args (and (or (eq type 'scalar) slot-list)
+                              (option? :scalar-args-version t)
+                              (expand-vector-lambda-list arglist))))
+        (cond ((eq type 'scalar)
+               (nconc (list 'progn)
+                      (emit-declarations type element-type name arglist :inline (option? :inline t))
+                      (emit-vector-function name arglist doc body)
+                      (when (option? :scalar-args-version t)
                         (nconc (emit-declarations type element-type scalar-args-name scalar-args)
                                (emit-plain-function scalar-args-name scalar-args
                                                     (concatenate 'string doc "
 Multiple values version. Takes the individual vector element as arguments.")
-                                                    body))))))
+                                                    body)))))
 
-            ;; Emit functions for a vector operation returning type not defined
-            ;; with defvector. This may be the types vec and mat.
-            ((null slot-list)
-             (nconc (list 'progn)
-                    (emit-declarations type element-type name arglist :inline (option? :inline nil))
-                    (emit-with-vectors-function name arglist doc body)))
+              ;; Emit functions for a vector operation returning type not defined
+              ;; with defvector. This may be the types vec and mat.
+              ((null slot-list)
+               (nconc (list 'progn)
+                      (emit-declarations type element-type name arglist :inline (option? :inline nil))
+                      (emit-vector-function name arglist doc body)))
 
-            ;; Emit functions for a vector operation returning a vector type
-            (t
-             (nconc (list 'progn)
-                    ;; main function
-                    (emit-declarations type element-type name arglist)
-                    (emit-multiple-value-call-function type name arglist doc body)
-                    ;; function returning multiple values
-                    (emit-declarations (slot-list-to-values element-type slot-list)
-                                       element-type mv-name arglist)
-                    (emit-with-vectors-function mv-name arglist doc body)
-                    ;; destructive function
-                    (when (and (option? :destructive-version t)
-                               (eq type (ensure-cadr (first arglist))))
-                      (nconc (emit-declarations type element-type destructive-name arglist)
-                             (emit-destructive-function destructive-name arglist
-                                                        (concatenate 'string doc "
-Will destructivly modify the the first parameter to contain the result
-of the operation.")
-                                                        body)))
-                    ;; function with flat arglist
-                    (when (option? :scalar-args-version t)
-                      (let ((scalar-args (expand-vector-lambda-list arglist)))
+              ;; Emit functions for a vector operation returning a vector type
+              (t
+               (nconc (list 'progn)
+                      ;; function with flat arglist
+                      (when (option? :scalar-args-version t)
                         (nconc (emit-declarations (slot-list-to-values element-type slot-list)
                                                   element-type scalar-args-name scalar-args)
                                (emit-plain-function scalar-args-name scalar-args
                                                     (concatenate 'string doc "
 Multiple values version. Takes individual vector components as arguments
 and returns the result as multiple values.")
-                                                    body))))))))))
+                                                    body)))
+
+                      ;; function returning multiple values
+                      (emit-declarations (slot-list-to-values element-type slot-list)
+                                         element-type mv-name arglist)
+                      (emit-vector-function mv-name arglist doc
+                                            (if (option? :scalar-args-version t)
+                                                `((,scalar-args-name ,@scalar-args))
+                                                body))
+
+                      ;; main function
+                      (emit-declarations type element-type name arglist)
+                      (emit-plain-function name arglist doc
+                                           `((multiple-value-call #',type
+                                               (,mv-name ,@(mapcar #'ensure-car arglist)))))
+
+                      ;; destructive function
+                      (when (and (option? :destructive-version t)
+                                 (eq type (ensure-cadr (first arglist))))
+                        (nconc (emit-declarations type element-type destructive-name arglist)
+                               (emit-vector-function destructive-name arglist
+                                                     (concatenate 'string doc "
+Will destructivly modify the the first parameter to contain the result
+of the operation.")
+                                                     `((setf (values ,@(expand-vector-arg (first arglist)))
+                                                             (,mv-name ,@(mapcar #'ensure-car arglist)))
+                                                       ,(ensure-car (first arglist)))))))))))))
 
 
 ;;; types.lisp ends here
